69fae5f109be3b797b78e803322e52861998342e
seblundin <seblundin@gmail.com>
2022-12-20 17:41:15 +0200

    A working version of fetching straight to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return <p>Pokemon stuff</p>;
+  const [pokemon, setPokemon] = useState([]);
+  const pokemons = 1154;
+  const slice = 20;
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${Math.random() * (pokemons - slice)}?limit=${slice}`)
+      .then(response => response.json())
+      .then(data => {
+        setPokemon(data.results);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {pokemon.map(p => <li key={p.url}> {p.name} </li>)}
+    </ul>
+  );
 };
 
 export default App;
